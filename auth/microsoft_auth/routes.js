3696fed0df1216af2e8d83efb7c91cebe16a263b
subhadip1203 <subhadip1203@gmail.com>
2023-03-16 15:11:54 -0700

    Feat: add eslint and prettier style formating

--- auth/microsoft_auth/routes.js
+++ auth/microsoft_auth/routes.js
@@ -4,25 +4,34 @@ const MicrosoftStrategy = require('passport-microsoft').Strategy;
 
 const router = express.Router();
 
-
-passport.use(new MicrosoftStrategy({
-  callbackURL: `http://localhost:3000/auth/microsoft/redirect`,
-  clientID: process.env.MICROSOFT_CLIENT_ID,
-  clientSecret: process.env.MICROSOFT_CLIENT_SECRET,
-  scope: ['user.read'],
-}, function (accessToken, refreshToken, profile, done) {
-  
-  const userId = profile.id 
-  let user_email = profile.emails && profile.emails[0].value; 
-  console.log("userId", userId)
-  console.log("user_email", user_email)
-  return done(null, {userId:userId ,user_email:user_email});
-}))
+passport.use(
+    new MicrosoftStrategy(
+        {
+            callbackURL: `http://localhost:3000/auth/microsoft/redirect`,
+            clientID: process.env.MICROSOFT_CLIENT_ID,
+            clientSecret: process.env.MICROSOFT_CLIENT_SECRET,
+            scope: ['user.read']
+        },
+        (accessToken, refreshToken, profile, done) => {
+            const userId = profile.id;
+            const userEmail = profile.emails && profile.emails[0].value;
+            console.log('userId', userId);
+            console.log('user_email', userEmail);
+            return done(null, { userId, userEmail });
+        }
+    )
+);
 
 router.get('/', passport.authenticate('microsoft', { session: false }));
-router.get('/redirect', passport.authenticate('microsoft', { session: false, failureRedirect: `https://localhost:3000/auth` }), (req, res) => {
-  res.send(req.user)
-});
-
+router.get(
+    '/redirect',
+    passport.authenticate('microsoft', {
+        session: false,
+        failureRedirect: `https://localhost:3000/auth`
+    }),
+    (req, res) => {
+        res.send(req.user);
+    }
+);
 
 module.exports = router;
