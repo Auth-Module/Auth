a5abc60a047d6462c31fa9b787aaa35f9cdcbd27
subhadip1203 <subhadip1203@gmail.com>
2023-03-20 09:54:33 -0700

    login with microsoft [AM-4]

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
+app.get('/auth', (req, res) => {
+    res.send('Auth Page');
+});
 app.use('/auth/microsoft/', require('./auth/microsoft_auth/routes'));
 
 const port = 3000;
