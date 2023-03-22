a4e20dc9688f09300825118a71f3b8f46b6a8e50
subhadip pahari <61578562+subhadip1203@users.noreply.github.com>
2023-03-22 11:28:56 -0700

    Login Page (#11)
    * Doc : crrating doc for githiub [AM-2]
    
    * Fix : creating doc for githiub [AM-2]
    
    * Fix : creating doc for github part 2  [AM-2]
    
    * Fix : creating doc for github part 3  [AM-2]
    
    * Feat: add eslint and prettier style formating
    
    * Feat: add eslint and prettier style formating [AM-3]
    
    * project structure [AM-1]
    
    * login with microsoft [AM-4]
    
    * login with google [AM-5]
    
    * login page [AM-6]

--- auth/google_auth/google_routes.js
+++ auth/google_auth/google_routes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const axios = require('axios');
 const router = express.Router();
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
@@ -9,13 +9,40 @@ passport.use(
         {
             clientID: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-            callbackURL: 'http://localhost:3000/auth/google/redirect'
+            callbackURL: `${process.env.PROJECT_BACKEND}/auth/google/redirect`
         },
-        (accessToken, refreshToken, profile, done) => done(null, profile)
+        (accessToken, refreshToken, profile, done) => {
+            axios
+                .get(`https://www.googleapis.com/oauth2/v3/tokeninfo?access_token=${accessToken}`)
+                .then((response) => {
+                    if (
+                        response.data.email === profile.emails[0]?.value &&
+                        profile.emails[0]?.verified === true
+                    ) {
+                        return done(null, {
+                            userId: profile.id,
+                            email: response.data.email,
+                            loginBy: 'google'
+                        });
+                    }
+
+                    return done('authentication failed', null);
+                })
+                .catch((err) => done(err, null));
+        }
     )
 );
 
-router.get('/', passport.authenticate('google', { scope: ['profile'] }));
+router.get(
+    '/',
+    passport.authenticate('google', {
+        scope: [
+            'https://www.googleapis.com/auth/userinfo.profile',
+            'https://www.googleapis.com/auth/userinfo.email'
+        ]
+    })
+);
+
 
 router.get(
     '/redirect',
--- index.js
+++ index.js
@@ -3,12 +3,15 @@ const express = require('express');
 
 const app = express();
 
+app.use('/static', express.static('static'));
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('ping');
 });
 
 app.get('/auth', (req, res) => {
-    res.send('Auth Page');
+    res.render('auth/index');
 });
 app.use('/auth/microsoft/', require('./auth/microsoft_auth/microsoft_routes'));
 app.use('/auth/google/', require('./auth/google_auth/google_routes'));
